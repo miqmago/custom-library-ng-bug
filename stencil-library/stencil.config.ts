7145589c9acc0e87d9ff84e83c286c136aa34863
Miquel Martín <miquel.martin@bitgenoma.com>
2023-09-07 20:04:43 +0200

    initial commit

---
+++ stencil-library/stencil.config.ts
@@ -0,0 +1,21 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    // By default, the generated proxy components will
+    // leverage the output from the `dist` target, so we
+    // need to explicitly define that output alongside the
+    // Angular target
+    {
+      type: 'dist',
+    },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      outputType: 'component',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
+  ],
+};
